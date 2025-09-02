04fe93981a6f47fe8a928c5e42f44d17ef7d9834
rock <rock@uwm.edu>
2025-09-01 20:52:11 -0500

    go

--- DjangoProject1/urls.py
+++ DjangoProject1/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from demo.views import Window
+from demo.views import Window, Away
 
 urlpatterns = [
-    path('', Window.as_view())
+    path('', Window.as_view(), name ="home"),
+    path('away/', Away.as_view(), name ="away")
 ]
--- demo/views.py
+++ demo/views.py
@@ -4,5 +4,8 @@ from django.shortcuts import render
 
 class Window(View):
     def get(self, request):
-        return render(request, "hello.html", {"color": "red", "data": "This is capybara", "TODD": 6})
+        return render(request, "hello.html", {"color": "red", "data": "This is fun"})
 
+class Away(View):
+    def get(self, request):
+        return render(request, "away.html")
